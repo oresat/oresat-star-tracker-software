ad7418b69719b7b527a5f8903deba5cf0990b652
ryanpdx <rmedick@pdx.edu>
2022-05-20 14:50:26 -0700

    add mock-hw arg

--- oresat_star_tracker/__main__.py
+++ oresat_star_tracker/__main__.py
@@ -11,7 +11,8 @@ from .star_tracker_resource import StarTrackerResource
 
 def main():
     # add the parent ArgumentParser for standard OreSat app args
-    parser = ArgumentParser(parents=[app_args_parser])
+    parser = ArgumentParser(prog='oresat-star-tracker', parents=[app_args_parser])
+    parser.add_argument('-m', '--mock-hw', action='store_true', help='mock the hardware')
     args = parser.parse_args()
     parse_app_args(args)  # parse the standard app args
 
@@ -19,7 +20,7 @@ def main():
 
     app = App(eds_file, args.bus, args.node_id)
 
-    resource = StarTrackerResource(app.node, app.fread_cache)
+    resource = StarTrackerResource(app.node, app.fread_cache, args.mock_hw)
 
     app.add_resource(resource)
 
