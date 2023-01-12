10e9c3f677d3b06e3cceb5bc3fac3fd22fad3112
Aakarsh Nair <aakarsh@pdx.edu>
2023-01-12 11:24:44 +0000

    Adding options to trigger capture.

--- scripts/trigger_capture.py
+++ scripts/trigger_capture.py
@@ -4,22 +4,31 @@ import argparse
 import os
 import time
 
-bus_id = "vcan0"
-node_id = '0x2C'
-capture_idx = '0x6002'
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Trigger a capture command.')
 
-    parser.add_argument('-n', '--ntimes', help='Number of captures to trigger.',
+    parser.add_argument('-r', '--repeat-count', help='Number of captures to trigger.',
                         type=int, default=1)
-    parser.add_argument(
-        '-s', '--sleep', help='Number of seconds to sleep between triggers.',
-        type=int, default=None)
+
+    parser.add_argument('-b', '--bus', help='Number of captures to trigger.',
+                        type=str, default='vcan0')
+
+    parser.add_argument('-n', '--node-id', help='Number of captures to trigger.',
+                        type=str, default='0x2C')
+
+    parser.add_argument('-c', '--capture_idx', help='Number of captures to trigger.',
+                        type=str, default='0x6002')
+
+    parser.add_argument('-s', '--sleep',
+                        help='Number of seconds to sleep between triggers.',
+                        type=int, default=1)
 
     args = parser.parse_args()
+    bus_id = args.bus
+    node_id = args.node_id
+    capture_idx = args.capture_idx
 
-    for i in range(args.ntimes):
+    for i in range(args.repeat_count):
         os.system(f"sudo olaf-sdo-transfer {bus_id} {node_id} 'w' {capture_idx}  0 u8")
 
         if args.sleep:
