f4961f23695d83e861a75da6afe0096919af44b2
Aakarsh Nair <aakarsh@pdx.edu>
2023-01-12 12:29:42 +0000

    Remove encode and decode methods.

--- oresat_star_tracker/client/star_tracker_client.py
+++ oresat_star_tracker/client/star_tracker_client.py
@@ -18,110 +18,6 @@ STARTRACKER_NODE_ID = 0x2C
 EDS_FILE = dirname(abspath(__file__)) + '/../../oresat_star_tracker/data/star_tracker.eds'
 
 
-class CANopenTypes(Enum):
-    '''All valid canopen types supported'''
-    b = auto()
-    i8 = auto()
-    u8 = auto()
-    i16 = auto()
-    u16 = auto()
-    i32 = auto()
-    u32 = auto()
-    i64 = auto()
-    u64 = auto()
-    f32 = auto()
-    f64 = auto()
-    s = auto()
-    d = auto()  # DOMAIN type
-
-
-DECODE_KEYS = {
-    CANopenTypes.b: '?',
-    CANopenTypes.i8: 'b',
-    CANopenTypes.u8: 'B',
-    CANopenTypes.i16: 'h',
-    CANopenTypes.u16: 'H',
-    CANopenTypes.i32: 'i',
-    CANopenTypes.u32: 'I',
-    CANopenTypes.i64: 'q',
-    CANopenTypes.f32: 'f',
-    CANopenTypes.f64: 'd',
-}
-
-
-def decode_value(raw_data, co_type):
-    '''
-    Decode can open value
-    '''
-    data = None
-    if co_type == CANopenTypes.b:
-        data = unpack('?', raw_data)
-    elif co_type == CANopenTypes.i8:
-        data = unpack('b', raw_data)
-    elif co_type == CANopenTypes.u8:
-        data = unpack('B', raw_data)
-    elif co_type == CANopenTypes.i16:
-        data = unpack('h', raw_data)
-    elif co_type == CANopenTypes.u16:
-        data = unpack('H', raw_data)
-    elif co_type == CANopenTypes.i32:
-        data = unpack('i', raw_data)
-    elif co_type == CANopenTypes.u32:
-        data = unpack('I', raw_data)
-    elif co_type == CANopenTypes.i64:
-        data = unpack('q', raw_data)
-    elif co_type == CANopenTypes.u64:
-        data = unpack('Q', raw_data)
-    elif co_type == CANopenTypes.f32:
-        data = unpack('f', raw_data)
-    elif co_type == CANopenTypes.f64:
-        data = unpack('d', raw_data)
-    elif co_type == CANopenTypes.s:
-        data = raw_data.decode('utf-8')
-        raise ValueError(data)
-    elif co_type == CANopenTypes.d:
-        raise ValueError(raw_data)
-    else:
-        raise ValueError(f'invalid data type:{co_type}')
-    return data
-
-
-def encode_value(value, co_type):
-    '''
-    Takes the value and a CAN open type end encodes
-    it for writing.
-    '''
-    if co_type == CANopenTypes.b:
-        raw_data = pack('?', int(value))
-    elif co_type == CANopenTypes.i8:
-        raw_data = pack('b', int(value))
-    elif co_type == CANopenTypes.u8:
-        raw_data = pack('B', int(value))
-    elif co_type == CANopenTypes.i16:
-        raw_data = pack('h', int(value))
-    elif co_type == CANopenTypes.u16:
-        raw_data = pack('H', int(value))
-    elif co_type == CANopenTypes.i32:
-        raw_data = pack('i', int(value))
-    elif co_type == CANopenTypes.u32:
-        raw_data = pack('I', int(value))
-    elif co_type == CANopenTypes.i64:
-        raw_data = pack('q', int(value))
-    elif co_type == CANopenTypes.u64:
-        raw_data = pack('Q', int(value))
-    elif co_type == CANopenTypes.f32:
-        raw_data = pack('f', float(value))
-    elif co_type == CANopenTypes.f64:
-        raw_data = pack('d', float(value))
-    elif co_type == CANopenTypes.s:
-        raw_data = value.encode('utf-8')
-    elif co_type == CANopenTypes.d:
-        raw_data = value
-    else:
-        raise RuntimeError('invalid data type')
-    return raw_data
-
-
 def connect(bus_id=DEFAULT_BUS_ID, node_id=STARTRACKER_NODE_ID):
     '''
     Connect to to the startracker node
@@ -138,8 +34,7 @@ def trigger_capture_star_tracker(sdo):
     Send the capture command.
     '''
     try:
-        payload = encode_value(1, CANopenTypes.i8)
-        sdo.download(0x6002, 0, payload)
+        sdo[0x6002].phys = 1
     except Exception as exc:
         traceback.print_exc()
         raise exc
@@ -147,14 +42,12 @@ def trigger_capture_star_tracker(sdo):
 
 def get_star_tracker_state(sdo):
     ''' Retreive tracker state.  '''
-    returned_value = sdo.upload(0x6000, 0)
-    decoded_state = decode_value(returned_value, CANopenTypes.i8)[0]
+    decoded_state = sdo[0x6000].phys
     return decoded_state
 
 
 def set_star_tracker_state(sdo, state_command):
-    payload = encode_value(state_command.value, CANopenTypes.i8)
-    sdo.download(0x6000, 0, payload)
+    sdo[0x6000].phys = state_command.value
     return True
 
 
