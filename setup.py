118f9b4281e7927d9b562a2b71f3366ec914a673
ryanpdx <rmedick@protonmail.com>
2023-11-05 08:52:03 -0800

    rework package build configs

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import Extension, setup
-from setuptools.command.build_py import build_py
+'''Package setup file. Only used for SWIG.'''
 
-from oresat_star_tracker import __version__
+from setuptools import Extension, setup  # type: ignore
+from setuptools.command.build_py import build_py  # type: ignore
 
 BEAST_EXT = Extension(
     name='_beast',
-    swig_opts=['-py3', '-c++'],
+    swig_opts=['-c++'],
     sources=[
         'oresat_star_tracker/beast/beast.i',
     ],
@@ -21,59 +21,15 @@ BEAST_EXT = Extension(
 # Build extensions before python modules,
 # or the generated SWIG python files will be missing.
 class BuildPy(build_py):
+    '''Class to add swig configs.'''
     def run(self):
         self.run_command('build_ext')
         super(build_py, self).run()
 
 
 setup(
-    name='oresat-star-tracker',
-    description='The OreSat Star Tracker app',
-    version=__version__,
-    license='GPLv3',
-    author='PSAS',
-    author_email='oresat@pdx.edu',
-    url='https://github.com/oresat/oresat-star-tracker-software',
-    keywords=['SWIG', 'oresat', 'star tracker'],
-    packages=['oresat_star_tracker'],
-    package_data={
-        'oresat_star_tracker': [
-            'beast/*.cpp',
-            'beast/*.cxx',
-            'beast/*.h',
-            'beast/*.i',
-            'beast/*.o',
-            'beast/*.so',
-            'beast/Makefile',
-            'data/*',
-            'templates/*',
-        ]
-    },
     ext_modules=[BEAST_EXT],
     cmdclass={
         'build_py': BuildPy,
     },
-    install_requires=[
-        'oresat-configs',
-        'oresat-olaf>=3.0.0',
-        'opencv-python-headless==4.6.0.66',
-        'swig',
-    ],
-    entry_points={
-        'console_scripts': [
-            'oresat-star-tracker = oresat_star_tracker.__main__:main',
-        ],
-    },
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Topic :: Software Development :: Embedded Systems',
-    ],
-    python_requires='>=3.7',
 )
