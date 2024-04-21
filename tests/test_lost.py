082a3a7484631ad5491e5c2f5ad8f8d882806d1a
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2024-04-21 16:36:30 -0700

    cleanup repo
    added some unit testing for Lost
    
    updated readme, cleaned repo a bit
    
    Delete external directory
    
    Delete cots_files directory
    
    remove NASA-COTS submodule
    
    remove NASACOTS

--- tests/test_lost.py
+++ tests/test_lost.py
@@ -1,20 +1,29 @@
-"""test LOST attitude solver"""
+"""
+test LOST attitude solver
+*must use sudo for Lost program to work
+"""
 
+import os
 import unittest
 
 import lost
 
 
-class Test_lost(unittest.TestCase):
+class TestLost(unittest.TestCase):
+    """Test the LOST star tracker solving algo"""
 
-    filename = "png_samples/capture-2022-10-16-12-55-56.png"
-    
-    data = lost.imread(filename)
-    lost_data = lost.identify(data, lost.identify_args(algo="tetra"))
-    
+    img_file = "./images/capture-2022-09-25-09-40-25.png"
+    current_dir = os.path.dirname(__file__)
+    path = os.path.join(current_dir, img_file)
+
+    data = lost.imread(path)
+    lost_args = lost.identify_args(algo="tetra")
+    lost_data = lost.identify(data, lost_args)
+
     print(lost_data["attitude_ra"])
     print(lost_data["attitude_de"])
     print(lost_data["attitude_roll"])
+
 
 if __name__ == "__main__":
     # unittest.main()
