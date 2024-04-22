b68699d8454a224d4d10989c96045526b75b2f9e
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2024-04-21 17:22:28 -0700

    remove NASA-COTS submodule

--- tests/test_lost.py
+++ tests/test_lost.py
@@ -3,14 +3,15 @@ test LOST attitude solver
 *must use sudo for Lost program to work
 """
 
-import unittest
 import os
+import unittest
 
 import lost
 
 
 class TestLost(unittest.TestCase):
     """Test the LOST star tracker solving algo"""
+
     img_file = "./images/capture-2022-09-25-09-40-25.png"
     current_dir = os.path.dirname(__file__)
     path = os.path.join(current_dir, img_file)
