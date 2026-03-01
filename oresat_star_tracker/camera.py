4b088d974b14f846fa816281cc49ce3450a5a53e
Cullen Sharp <cullen.sharp100@gmail.com>
2026-03-01 15:42:17 -0800

    Replace cv2 with numpy and PIL
    Significantly improves build times. All images are demosaiced; this
    removes the waffling pattern and adds color information back into the
    image. I opted to avoid white balancing or gamma correction, as those can
    be handled on a host machine.

--- oresat_star_tracker/camera.py
+++ oresat_star_tracker/camera.py
@@ -2,7 +2,7 @@
 
 from enum import Enum
 from pathlib import Path
-from colour_demosaicing import demosaicing_CFA_Bayer_bilinear
+from colour_demosaicing import demosaicing_CFA_Bayer_Malvar2004 
 import numpy as np
 from olaf import logger
 
@@ -37,7 +37,7 @@ class Camera:
             return
 
         # no errors; attempt to read image
-        context_path = Path("/sys/devices/platform/prucam/context_settings")
+        context_path = Path("/sys/devices/platform/prudev/context_settings")
         x_size = int((context_path / "x_size").read_text())
         y_size = int((context_path / "y_size").read_text())
         self._image_size = (y_size, x_size)
@@ -65,26 +65,24 @@ class Camera:
 
         if self._state != CameraState.RUNNING:
             raise CameraError(f"Camera error; state is {self._state}")
+        
+        try:
+            with open(self.CAPTURE_PATH, "rb") as f:
+                # read one 1280x960 frame
+                raw_bytes = f.read(self.PIXEL_BYTES)
 
-        img = np.fromfile(self.CAPTURE_PATH, dtype=(np.uint8, self._image_size), count=1)[0]
-        raw = img if img.ndim == 2 else img[..., 0]
+                raw = np.frombuffer(raw_bytes, dtype=np.uint8).reshape((self.MAX_ROWS,self.MAX_COLS))
+                logger.info(f"Successfuly captured {raw.shape}")
+        except Exception as e:
+            logger.error(f"Error: {e}")
 
         # normalize
-        raw = raw.astype(np.float32)
-        raw /= raw.max()
+        raw = raw.astype(np.float32) / 255
 
         # demosaic
-        rgb = demosaicing_CFA_Bayer_bilinear(raw)
+        rgb = demosaicing_CFA_Bayer_Malvar2004(raw)
 
-        # apply white balancing
-        rgb[..., 0] *= 1.4  # R gain
-        rgb[..., 1] *= 1.0  # G gain
-        rgb[..., 2] *= 1.7  # B gain
-
-        # clip
-        rgb = np.clip(rgb, 0, 1)
-
-        return (rgb * 255).astype(np.uint8)
+        return np.clip(rgb * 255, 0, 255).astype(np.uint8)
 
     @property
     def state(self) -> CameraState:
